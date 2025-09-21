3b2fe028ee7d1ac7655172b14793dfc53219537e
thanhhuynh1609 <hnkt1609@gmail.com>
2025-09-21 11:34:19 +0700

    fix login docker 4

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-const http = require('http');
+// const http = require('http');
 
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello CI/CD with Docker & GitHub Actions!\n');
+// });
+
+// server.listen(port, () => {
+//   console.log(`Server running at http://localhost:${port}/`);
+// });
+
+const http = require("http");
+
+const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello CI/CD with Docker & GitHub Actions!\n');
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello CI/CD with Docker & GitHub Actions!\n");
 });
 
-server.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+// Lắng nghe trên tất cả IP (0.0.0.0)
+server.listen(port, "0.0.0.0", () => {
+  console.log(`Server running at http://0.0.0.0:${port}/`);
 });
