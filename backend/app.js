d8d4b845294bc315473dcc100ed0c14546d37faf
thanhhuynh1609 <hnkt1609@gmail.com>
2025-09-23 21:42:14 +0700

    fix cors

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
+
+
+
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
