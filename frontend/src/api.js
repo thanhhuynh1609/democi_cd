4407b3a1627bbcfdb08939617315e45387b1c36f
thanhhuynh1609 <hnkt1609@gmail.com>
2025-09-23 21:08:18 +0700

    fix docker-compose 7

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000" // chỉnh sang http://backend:3000 trong docker-compose
+  baseURL: "http://backend:3000" // chỉnh sang http://backend:3000 trong docker-compose
 });
 
 export default API;
